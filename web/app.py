528a94ce4d8756effdbf0cd2074e0def2705dd51
VictorBarros <victorbarros1130@gmail.com>
2023-03-06 19:16:29 -0500

    add healthcheck

--- web/app.py
+++ web/app.py
@@ -132,6 +132,12 @@ def linkedin():
         </html>
     """
 
+
+@app.route("/health-check", methods=["GET"])
+def health_check():
+    return {}, 200
+
+
 if __name__ == "__main__":
     app.run(port=int(os.environ.get("PORT", 80)), host="0.0.0.0", debug=True)
     # https://victoralmeidabarros.com/
