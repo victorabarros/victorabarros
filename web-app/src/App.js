a73a9187d135855e4d8dd17defcbbab905a01746
VictorBarros <victorbarros1130@gmail.com>
2023-12-26 19:33:53 -0300

    add makefile and refact src

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,70 +1,27 @@
 import React from "react"
 import { StyleSheet, Text, View, ScrollView } from "react-native"
+import { sideProjects, articles, certifications, openSourceContributions } from "./data"
+import { AntDesign } from "react-native-vector-icons/AntDesign"
 
-const sideProjects = [
-  {
-    name: "UraBot",
-    url: "https://twitter.com/UraniumStockBot"
-  },
-  {
-    name: "YooMoney",
-    url: "https://twitter.com/YooMoneyApp"
-  }
-]
-const articles = [
-  {
-    name: "NSQ with Docker in baby steps less than 70 lines of code",
-    url: "https://victoralmeidabarros.medium.com/nsq-with-docker-in-baby-steps-70-lines-of-code-381ac37eaf58"
-  },
-  {
-    name: "Developing a MiniApp for the Klutch Credit Card",
-    url: "https://victoralmeidabarros.medium.com/developing-a-miniapp-for-the-klutch-credit-card-df33f014183e"
-  },
-  {
-    name: "Install Docker with one single command (Debian and Ubuntu)",
-    url: "https://victoralmeidabarros.medium.com/install-docker-with-one-single-command-debian-and-ubuntu-ce021df0f976"
-  },
-]
-const certifications = [
-  {
-    name: "CS50, Harvard",
-    url: "https://certificates.cs50.io/3fc935e2-a295-47c9-9dd4-3658ddfb84e7"
-  },
-  {
-    name: "English, Duolingo",
-    url: "https://certs.duolingo.com/708c9de8137952ac95900efb90a31002"
-  },
-]
-const openSourceContributions = [
-  {
-    name: "w3b3d3v/w3d-podcast",
-    url: "https://github.com/w3b3d3v/w3d-podcast/pull/1"
-  },
-  {
-    name: "dgrijalva/jwt-go",
-    url: "https://github.com/dgrijalva/jwt-go/issues/450"
-  },
-  {
-    name: "nsqio/nsq",
-    url: "https://github.com/nsqio/nsq/pull/1243"
-  },
-  {
-    name: "nsqio/nsqio.github.io",
-    url: "https://github.com/nsqio/nsqio.github.io/pull/60"
-  },
-  {
-    name: "PacktPublishing/Learn-Go-in-3-Hours",
-    url: "https://github.com/PacktPublishing/Learn-Go-in-3-Hours/pull/3"
-  },
-]
-
-const Link = ({ name, url }) => <Text href={url} style={styles.linkText}>{name}</Text>
+const Link = ({ name, url, description }) => (
+  <View style={styles.linkContainer}>
+    <Text href={url} style={styles.linkText}>{name}</Text>
+    <Text style={styles.linkdescriptionText}>{"\t" + description}</Text>
+    {/* <AntDesign
+      name="star"
+      color={"black"}
+      size={20}
+    // style={[styles.icon, { marginTop: size / 2 }]}
+    /> */}
+  </View>
+)
 
 const Line = () => (
   <View style={{ height: 20, overflow: "hidden", marginVertical: 30 }}>
     <Text style={{ color: "lightgrey" }}>{"=".repeat(1000)}</Text>
   </View>
 )
+
 const App = () => (
   <View style={styles.root}>
 
@@ -78,27 +35,21 @@ const App = () => (
 
         <Line />
 
-        <View style={styles.chapterContainer}>
-          <Text style={styles.labelText}>Side Projects</Text>
-          {sideProjects.map(Link)}
-        </View>
+        {[
+          ["Side Projects", sideProjects],
+          ["Articles", articles],
+          ["Certifications", certifications],
+          ["Open Source Contributions", openSourceContributions],
+        ].map(([headLine, links]) => (
+          <View style={styles.chapterContainer}>
+            <Text style={styles.labelText}>{headLine}</Text>
+            {links.map(Link)}
+          </View>
+        ))}
 
-        <View style={styles.chapterContainer}>
-          <Text style={styles.labelText}>Articles</Text>
-          {articles.map(Link)}
-        </View>
-
-        <View style={styles.chapterContainer}>
-          <Text style={styles.labelText}>Certifications</Text>
-          {certifications.map(Link)}
-        </View>
-
-        <View style={styles.chapterContainer}>
-          <Text style={styles.labelText}>Open Source Contributions</Text>
-          {openSourceContributions.map(Link)}
-        </View>
-
-        {/* TODO add social medias */}
+        {/* TODO add social medias
+        resume: https://s3.amazonaws.com/attachments.angel.co/9210750-fbec722a76b0d4ba5e2d765e896bea0d.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIATAVHNKYQ7SKKADZS%2F20231006%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231006T180725Z&X-Amz-Expires=3600&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEHIaCXVzLXdlc3QtMiJGMEQCIBHaL2pIgxraEwjKcAujiGZFfw9lM43WiP4%2F8Iv0sNnqAiBAa5wJQi8Eck2S3hGA8MfRoaylqQVaX8nts04ajdxRzCqLBQh7EAAaDDIwNzU4MzI3MDQzMyIMKTZkI2RGR1zL%2FmJWKugEsosCH%2BdGK%2BfyEkBhcDgG7D6g5WCcnOIuc9jxMbfkVnYtaCHGp69brb2uT%2FfaGz3wdhS7bTlztQgWwjoZvsjrLMEAUbFQQsyZ%2Bhps%2FQvU3eA7nLyaElQolGcHqleQXKX%2FPejzSi5uApew8tjCsqia24xNvG42BKfMSKZdUfI2TonsSMxgsdff%2FVTfOlLXvtSSB6r8Q3pP5W5OU4KLIAdQK2MHksyBqZjnKD9QO2ENrY5I3ah1E5lz%2Bysi9z2%2FeLD0Lg0b%2BthMn2IMbH4XlqJ4h2xHIPLMGG0Tkw1pmVT792%2BbW6baCBjP54ogU3RLo8RfeHbHe9NN74v%2F7ARW0YoDvpDgTSuMzdBWyzgbAI6Ow%2FwKGsh9t2u2YnOPjW3YhY%2BosjvwKa5ekN0LOsb%2Bcmi92MZffeP1pZXtsa8Uq8PAIK3ZLrqVP4gLCQ3S7jwv4KcwZuJsM%2FPq13cKp54%2FNQK2PR8Nqya2bcb57abDR9zUe2Gj3IZKlGABE2XceBVUvd6S%2FpM3lyMVZkr5Ng80tJdAcmHU6yDS5MhahamUnaZvbb7PWcZfJzD%2FVQOTeckkwr3mP2PsE9CxfZ2AcPvHYs%2BjPBzkCOIhah1uNZK%2Fz6njtu5oFOsHnV6esP7GUwS61vyFhFIjDAhRzc0YvloESiTk%2FheOM9TmWdlqvsV6ze6IfbYxjToRTmYgvQqdU84xuNFAA55qZicL3kwfrcC2oaVylYT%2Bv47KlPrtmvZIKo8oFzc1ot%2B0ADtktJtqGNSSlhfXCKDMSfwBAA95ygt2hZx3rb826Mng7ylREYfHUz5uR15no%2F%2Bb0VghQzDlkIGpBjqbAUjSYFiKnjPuhU92AYe04FE9m2cVHceAGi94LWzOYsrUMBdcHZtx4lY%2BXNFaZzbtCpZu8YxZUiQcSZJVrp%2FD6GHRRTcpukTzcH%2FuNLHf1SDIo%2FpsdaMq3AJzAB6YzTFT1Qn%2BSjYezMXrAUVsyCdh%2FmO%2FftULUhuxqc59%2FSkWnblqaNfQaJmSTIPYrF604pAbyM3rbnHhFtjiv4UM&X-Amz-SignedHeaders=host&X-Amz-Signature=d85b725b3e762a481446f5c61775bcc70839148ef93ff2145c0e70bbfd1351f2
+        */}
 
         <Line />
 
@@ -142,11 +93,20 @@ const styles = StyleSheet.create({
     lineHeight: 50,
     fontSize: 30,
   },
+  linkContainer: {
+    marginBottom: 10,
+  },
   linkText: {
-    fontSize: 20,
+    fontSize: 30,
     lineHeight: 40,
     color: 'rgb(255, 196, 71)',
     alignSelf: "flex-start",
+  },
+  linkdescriptionText: {
+    fontSize: 20,
+    // lineHeight: 40,
+    color: 'rgb(255, 196, 71)',
+    opacity: .8
   },
   footerText: {
     color: "white",
---
+++ web-app/src/data.js
@@ -0,0 +1,71 @@
+export const sideProjects = [
+  {
+    name: "UraBot",
+    description: "Sharing uranium market stock prices, analyses and relevant news on Twiiter",
+    url: "https://twitter.com/UraniumStockBot"
+  },
+  {
+    name: "YooMoney",
+    description: "Funny way to learn about money",
+    url: "https://twitter.com/YooMoneyApp"
+  }
+]
+
+export const articles = [
+  {
+    name: "NSQ with Docker in baby steps less than 70 lines of code",
+    description: "Learn how to run and manage the NSQ messaging platform with few lines of code",
+    url: "https://victoralmeidabarros.medium.com/nsq-with-docker-in-baby-steps-70-lines-of-code-381ac37eaf58"
+  },
+  {
+    name: "Developing a MiniApp for the Klutch Credit Card",
+    description: "How to develop a miniapp to Klutch's market place",
+    url: "https://victoralmeidabarros.medium.com/developing-a-miniapp-for-the-klutch-credit-card-df33f014183e"
+  },
+  {
+    name: "Install Docker with one single command (Debian and Ubuntu)",
+    description: "The easist way of how to install docker in your VM",
+    url: "https://victoralmeidabarros.medium.com/install-docker-with-one-single-command-debian-and-ubuntu-ce021df0f976"
+  },
+]
+
+export const certifications = [
+  {
+    name: "CS50, Harvard",
+    description: "Harvard's introduction to the intellectual enterprises of computer science and programming",
+    url: "https://certificates.cs50.io/3fc935e2-a295-47c9-9dd4-3658ddfb84e7"
+  },
+  {
+    name: "English, Duolingo",
+    description: "",
+    url: "https://certs.duolingo.com/708c9de8137952ac95900efb90a31002"
+  },
+]
+
+export const openSourceContributions = [
+  {
+    name: "w3b3d3v/w3d-podcast",
+    description: "Using asynchronous calls to improve performance",
+    url: "https://github.com/w3b3d3v/w3d-podcast/pull/1"
+  },
+  {
+    name: "dgrijalva/jwt-go",
+    description: "Token generation method isn't working",
+    url: "https://github.com/dgrijalva/jwt-go/issues/450"
+  },
+  {
+    name: "nsqio/nsq",
+    description: "Fix docker hub URL",
+    url: "https://github.com/nsqio/nsq/pull/1243"
+  },
+  {
+    name: "nsqio/nsqio.github.io",
+    description: "Fix docker hub url",
+    url: "https://github.com/nsqio/nsqio.github.io/pull/60"
+  },
+  {
+    name: "PacktPublishing/Learn-Go-in-3-Hours",
+    description: "Add forgotten lines",
+    url: "https://github.com/PacktPublishing/Learn-Go-in-3-Hours/pull/3"
+  },
+]
