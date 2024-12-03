f4ad88f2c02b610100309e86e918a41aeec13200
Victor Barros <victorbarros1130@gmail.com>
2024-12-02 22:42:27 -0500

    add icons for social medias

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { Pressable } from 'react-native-web';
-import { theme } from '../styles';
+import { theme , icons} from '../styles';
 import { SIDE_PROJECTS, ARTICLES, OPEN_SOURCE_CONTRIBUTIONS, CERTIFICATIONS, PROFESSIONAL_ASSOCIATIONS, SOCIAL_MEDIAS } from '../data';
 
 const Item = ({id, label, url}) =>(
-    <Pressable key={id-label} style={styles.itemTextContainer} onPress={() => window.open(url)}>
+    <Pressable style={styles.itemTextContainer} onPress={() => window.open(url)}>
     <Text style={styles.itemText} >{label}</Text>
     </Pressable>
   )
@@ -31,9 +31,9 @@ function HorizontalLine() {
 }
 
 const SocialMedias = ({id, url, icon}) => (
-    <View>
-      <Text key={id-url}>{url}</Text>
-    </View>
+    <Pressable onPress={() => window.open(url)}>
+          <Image style={styles.socialMediasImage} source={icons[icon].svg} />
+    </Pressable>
   )
 
 export default function IndexPage() {
@@ -49,53 +49,67 @@ export default function IndexPage() {
       color: theme.gray,
   }]}>
         Currently working at
-        <Text style={{
-      color: theme.orange,
-        }}
+        <Pressable        onPress={() => window.open('https://superhuman.com/')}        >
+        <Text style={{          color: theme.orange,        }}
         > Superhuman</Text>
+        </Pressable>
         </Text>
 </View>
 
 <View style={styles.section}>
         <Text style={styles.title}>Side Projects</Text>
+
           {SIDE_PROJECTS.map((item) => (
-            <Item {...item} />
+            <Item  key={item.id-item.label} {...item} />
           ))}
 </View>
+
 
 <View style={styles.section}>
         <Text style={styles.title}>Articles</Text>
           {ARTICLES.map((item) => (
-            <Item {...item} />
+            <Item key={item.id-item.label} {...item} />
           ))}
 </View>
 
 <View style={styles.section}>
         <Text style={styles.title}>Open Source Contributions</Text>
           {OPEN_SOURCE_CONTRIBUTIONS.map((item) => (
-            <Item {...item} />
+            <Item key={item.id-item.label} {...item} />
           ))}
 </View>
 
 <View style={styles.section}>
         <Text style={styles.title}>Certifications</Text>
           {CERTIFICATIONS.map((item) => (
-            <Item {...item} />
+            <Item key={item.id-item.label} {...item} />
           ))}
 </View>
 
 <View style={styles.section}>
         <Text style={styles.title}>Professional Membership</Text>
           {PROFESSIONAL_ASSOCIATIONS.map((item) => (
-            <Item {...item} />
+            <Item key={item.id-item.label} {...item} />
           ))}
 </View>
 
         <HorizontalLine />
 
+<View style={styles.socialMediasContainer}>
+
         {SOCIAL_MEDIAS.map((item) => (
-          <SocialMedias {...item} />
+          <SocialMedias key={item.id-item.label} {...item} />
         ))}
+        </View>
+
+
+<View style={styles.footerContainer}>
+<Text style={styles.footer}>© Victor Barros - Built with
+  <Pressable        onPress={() => window.open('https://necolas.github.io/react-native-web/')}        >
+  {' React Native for Web'}
+  </Pressable>
+  </Text>
+</View>
 
     </View>
   );
@@ -132,6 +146,23 @@ marginVertical: 15,
   itemText: {
     color: theme.orange,
     fontSize: 22,
+    fontFamily: 'Times New Roman',
+  },
+  socialMediasContainer:{
+flexDirection: 'row',
+alignSelf: 'flex-start',
+width: '65%',
+justifyContent: 'space-between',
+  },
+  socialMediasImage: {
+    margin: 10,
+    width: 55,
+    height: 55,
+  },
+  footerContainer:{alignSelf: 'center', marginTop: 30},
+  footer:{
+    color: theme.gray,
+    fontSize: 16,
     fontFamily: 'Times New Roman',
   },
 });
