3459ae5965ad937c5bdca28407516c0ba0b99e77
Victor <victorbarros1130@gmail.com>
2026-06-05 13:30:53 -0400

    🦙 ollama generated

--- scripts/install_docker_ubuntu.sh
+++ scripts/install_docker_ubuntu.sh
@@ -2,7 +2,6 @@
 sudo apt remove $(dpkg --get-selections docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc | cut -f1)
 
 # https://docs.docker.com/engine/install/ubuntu/#install-using-the-convenience-script
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh ./get-docker.sh --dry-run
+curl -fsSL https://get.docker.com | sudo sh
 
 sudo docker run hello-world
